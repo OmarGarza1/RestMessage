d6d256369fe251ce338f9b350604aacb3926479d
Greencho <Greencho@DESKTOP-0KIIREV>
2020-01-14 13:14:38 -0600

    Message Rest Service

---
+++ messenger/src/main/java/org/omar/javabrains/messenger/exception/DataNotFoundException.java
@@ -0,0 +1,15 @@
+package org.omar.javabrains.messenger.exception;
+
+public class DataNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6829099215819289663L;
+
+	 
+	public DataNotFoundException(String message) 
+	{
+		super(message);
+	}
+}
